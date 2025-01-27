3fd91a60db8df1b5ad2d26a269b7096534c1b6d0
Kacper Urbanowicz <Kacper.Urbanowicz@reconext.com>
2025-01-27 08:09:06 +0100

    Zmiana funkcjonalnosci. Dodanie dwoch przyciskow PASS/FAIL do decyzji Operatora. Numer seryjny wysyla sie do zapisu dopiero po ocenie. Dodano kolumne z wynikiem do pliku Excel.

--- .venv/Main-1.py
+++ .venv/Main-1.py
@@ -9,7 +9,8 @@ from datetime import datetime
 VALID_HRID = {
     "44963", "12100667", "81705", "45216", "45061", "12100171",
     "12100741", "81560", "81563", "81564", "45233", "12101333",
-    "12101111",
+    "12101111", "12100174", "12100475", "12101090", "12100587",
+    "12101094", "45016"
 }
 
 # Ustawienia pliku
@@ -24,33 +25,16 @@ def zatwierdz_hrid():
         entry_serial.config(state="normal")
         entry_serial.focus()  # Ustawiamy kursor w polu numeru seryjnego
         label_serial.config(state="normal")
+        # Pokazujemy przyciski PASS i FAIL
+        button_pass.config(state="normal")
+        button_fail.config(state="normal")
         # Zablokowanie pola HRID po zatwierdzeniu
         entry_hrid.config(state="disabled")
         messagebox.showinfo("Informacja", f"HRID {hrid} zatwierdzony. Możesz teraz wprowadzić numer seryjny.")
     else:
         messagebox.showwarning("Ostrzeżenie", "Wprowadziłeś nieprawidłowy HRID. Spróbuj raz jeszcze.")
 
-def zatwierdz_serial(event=None):
-    serial = entry_serial.get()
-    hrid = entry_hrid.get()
-
-    # Sprawdzamy, czy numer seryjny ma dokładnie 12 znaków
-    if len(serial) == 12:
-        # Pobieramy aktualną datę i czas
-        current_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-        # Zapisujemy dane do pliku CSV
-        zapis_do_csv(hrid, serial, current_date)
-
-        # Wyświetlenie komunikatu o zatwierdzeniu numeru seryjnego (zielony, pogrubiony)
-        show_message("Numer seryjny zatwierdzony.", "green")
-        entry_serial.delete(0, tk.END)  # Czyszczenie pola numeru seryjnego po zapisaniu
-        entry_serial.focus()  # Ustawienie kursora w polu numeru seryjnego
-    elif len(serial) > 12:
-        # Wyświetlenie ostrzeżenia (czerwony, pogrubiony)
-        show_message("Numer seryjny nie może mieć więcej niż 12 znaków!", "red")
-
-def zapis_do_csv(hrid, serial, date):
+def zapis_do_csv(hrid, serial, date, wynik):
     global file_counter, current_file
     original_file = "dane.csv"  # Główny plik
 
@@ -79,11 +63,29 @@ def zapis_do_csv(hrid, serial, date):
             writer = csv.writer(file)
             # Jeśli plik jest pusty, dodajemy nagłówki
             if current_row_count == 0:
-                writer.writerow(["HRID", "Numer seryjny", "Data"])
-            writer.writerow([hrid, serial, date])
+                writer.writerow(["HRID", "Numer seryjny", "Data", "Wynik"])
+            writer.writerow([hrid, serial, date, wynik])
     except Exception as e:
         # Wyświetlenie błędu (czerwony, pogrubiony)
         show_message("Nie udało się zapisać danych", "red")
+
+def handle_result(result):
+    serial = entry_serial.get()
+    hrid = entry_hrid.get()
+
+    if len(serial) == 12:
+        # Pobieramy aktualną datę i czas
+        current_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        # Zapisujemy dane do pliku CSV
+        zapis_do_csv(hrid, serial, current_date, result)
+
+        # Wyświetlenie komunikatu o zatwierdzeniu numeru seryjnego
+        show_message(f"Numer seryjny  {result}.", "green" if result == "PASS" else "red")
+        entry_serial.delete(0, tk.END)  # Czyszczenie pola numeru seryjnego po zapisaniu
+        entry_serial.focus()  # Ustawienie kursora w polu numeru seryjnego
+    else:
+        show_message("Numer seryjny musi mieć dokładnie 12 znaków!", "red")
 
 def show_message(message, color):
     # Wyświetlenie komunikatu w etykiecie z odpowiednim kolorem i stylem
@@ -100,6 +102,8 @@ def wyloguj():
     entry_hrid.config(state="normal")
     entry_serial.config(state="disabled")
     label_serial.config(state="disabled")
+    button_pass.config(state="disabled")
+    button_fail.config(state="disabled")
     entry_hrid.delete(0, tk.END)  # Usunięcie wprowadzonego HRID
     messagebox.showinfo("Wylogowanie", "Zostałeś wylogowany. Wprowadź nowy HRID, aby rozpocząć pracę.")
     entry_hrid.focus()  # Ustawienie kursora w polu HRID po wylogowaniu
@@ -107,16 +111,16 @@ def wyloguj():
 # Tworzenie głównego okna
 root = tk.Tk()
 root.title("Serializacja SP171")
-root.geometry("500x350")
+root.geometry("500x400")
 
 # Label i pole tekstowe dla HRID
 label_hrid = tk.Label(root, text="Wprowadź swój numer HRID, żeby rozpocząć pracę:", font=("Helvetica", 14, "bold"))
 label_hrid.pack(pady=10)
 
 # Pole tekstowe dla HRID
-entry_hrid = tk.Entry(root, font=("Helvetica", 16), width=18)  # Zwiększenie czcionki i szerokości
+entry_hrid = tk.Entry(root, font=("Helvetica", 16), width=18)
 entry_hrid.pack(pady=5)
-entry_hrid.focus()  # Ustawienie kursora w polu HRID
+entry_hrid.focus()
 
 # Przycisk zatwierdzenia HRID
 button_hrid = tk.Button(root, text="Zatwierdź", font=("Helvetica", 12, "bold"), command=zatwierdz_hrid)
@@ -125,33 +129,28 @@ button_hrid.pack(pady=10)
 # Label i pole tekstowe dla numeru seryjnego
 label_serial = tk.Label(root, text="Zeskanuj numer seryjny (12 znaków):", font=("Helvetica", 14, "bold"))
 label_serial.pack(pady=10)
-label_serial.config(state="disabled")  # Początkowo pole jest nieaktywne
+label_serial.config(state="disabled")
 
 # Pole tekstowe dla numeru seryjnego
-entry_serial = tk.Entry(root, font=("Helvetica", 16), width=18, state="disabled")  # Zwiększenie czcionki i szerokości
+entry_serial = tk.Entry(root, font=("Helvetica", 16), width=18, state="disabled")
 entry_serial.pack(pady=5)
 
-# Label do wyświetlania komunikatów
-label_message = tk.Label(root, text="", font=("Helvetica", 12))
-label_message.pack(pady=10)
+# Przyciski PASS i FAIL
+button_pass = tk.Button(root, text="PASS", font=("Helvetica", 12, "bold"), bg="green", fg="white", state="disabled",
+                        command=lambda: handle_result("PASS"))
+button_pass.pack(side="left", padx=50, pady=20)
 
-# Związanie naciśnięcia klawisza (z wyjątkiem Enter) z zatwierdzeniem numeru seryjnego
-entry_serial.bind("<KeyRelease>", zatwierdz_serial)
+button_fail = tk.Button(root, text="FAIL", font=("Helvetica", 12, "bold"), bg="red", fg="white", state="disabled",
+                        command=lambda: handle_result("FAIL"))
+button_fail.pack(side="right", padx=50, pady=20)
+
+# Label do wyświetlania komunikatów
+label_message = tk.Label(root, text="", font=("Helvetica", 12), width=50)  # Zwiększono szerokość
+label_message.pack(pady=10)
 
 # Przycisk wylogowania (w lewym dolnym rogu)
 button_wyloguj = tk.Button(root, text="Wyloguj", font=("Helvetica", 12, "bold"), command=wyloguj)
-button_wyloguj.place(x=10, y=305)
-
-# Ładowanie obrazu (logo)
-image_path = r"C:\Users\Kacper.Urbanowicz\PycharmProjects\PSU-Scanning\.venv\logo.png"  # Ścieżka do obrazu
-img = Image.open(image_path)
-img = img.resize((105, 35), Image.Resampling.LANCZOS)  # Zmieniamy rozmiar obrazka
-photo = ImageTk.PhotoImage(img)
-
-# Label z obrazkiem w prawym dolnym rogu
-label_logo = tk.Label(root, image=photo)
-label_logo.image = photo  # Przechowujemy referencję do obrazu, aby go nie zgubić
-label_logo.place(x=385, y=305)  # Ustawienie w prawym dolnym rogu
+button_wyloguj.place(x=10, y=360)
 
 # Uruchomienie pętli głównej
 root.mainloop()
